e0bf636997610d29fe7991fec6cc0f3f021534b0
Felipe Cacezi <felipecacezi@gmail.com>
2020-04-30 19:30:53 -0300

    Updates

---
+++ precache-manifest.3ad973f40e55179caec9e999011bf45e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a043c419c8143d621a490fb73161f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48e2a7ec47d364eadd47",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0a640d0e787298cb6ab5",
+    "url": "/static/css/main.baa81595.chunk.css"
+  },
+  {
+    "revision": "48e2a7ec47d364eadd47",
+    "url": "/static/js/2.d7908ec5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d7908ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a640d0e787298cb6ab5",
+    "url": "/static/js/main.00b24038.chunk.js"
+  },
+  {
+    "revision": "07ad4eedd5e91021597b",
+    "url": "/static/js/runtime-main.874e6ae7.js"
+  },
+  {
+    "revision": "2841cba23daf82fda6ff67311f3c0b0b",
+    "url": "/static/media/logoTitle.2841cba2.png"
+  }
+]);
